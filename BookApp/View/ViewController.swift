346b7355bb6a07758fee2b921689b2f0fc93d654
EllGHT <rmsgh1188@gmail.com>
2025-05-08 19:26:24 +0900

    feat: 프로젝트 세팅

---
+++ BookApp/View/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BookApp
+//
+//  Created by 정근호 on 5/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(#function)
+    }
+}
