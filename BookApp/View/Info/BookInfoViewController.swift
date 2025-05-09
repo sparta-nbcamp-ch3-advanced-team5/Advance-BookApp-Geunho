fd98efac87fa475c3659b9c7bd1fd4f7b656c866
EllGHT <rmsgh1188@gmail.com>
2025-05-09 15:48:19 +0900

    docs: 책 상세 뷰 파일 생성

---
+++ BookApp/View/Info/BookInfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BookInfoViewController.swift
+//  BookApp
+//
+//  Created by 정근호 on 5/9/25.
+//
+
+import UIKit
+import SnapKit
+
+class BookInfoViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .secondarySystemBackground
+    }
+}
