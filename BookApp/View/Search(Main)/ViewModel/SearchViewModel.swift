7cc057f04469b7869305f124f3bb63a031a74fb5
EllGHT <rmsgh1188@gmail.com>
2025-05-14 19:34:45 +0900

    feat: 무한 스크롤 로직 구현

--- BookApp/View/Search(Main)/ViewModel/SearchViewModel.swift
+++ BookApp/View/Search(Main)/ViewModel/SearchViewModel.swift
@@ -13,8 +13,14 @@ final class SearchViewModel {
     let disposeBag = DisposeBag()
     let searchedBookSubject = BehaviorSubject(value: [Book]())
     let recentBookSubject = BehaviorSubject(value: [Book]())
+    let metaData = BehaviorSubject(value: MetaData(isEnd: true, pageableCount: 0, totalCount: 0))
     /// 한 페이지에 보여질 문서 수, 1~50 사이의 값, 기본 값 10
-    let size = 50
+    let size = 10
+    
+    var isLoading = false
+    var page = 1
+    var endScroll = false
+    var isEnd = false
     
     private let coreDataManager: RecentBookStorageManager = CoreDataManager.shared
     
@@ -23,12 +29,29 @@ final class SearchViewModel {
     }
     
     func searchBooks() {
-        guard let url = URL(string: "https://dapi.kakao.com/v3/search/book?query=\(searchingText)&size=\(size)")
+        isLoading = true
+        
+        print("searchingPage: \(page)")
+        guard let url = URL(string: "https://dapi.kakao.com/v3/search/book?query=\(searchingText)&size=\(size)&page=\(page)")
         else { return }
         
         NetworkManager.shared.fetch(url: url)
             .subscribe(onSuccess: { [weak self] (bookResponse: BookResponse) in
-                self?.searchedBookSubject.onNext(bookResponse.documents)
+                self?.metaData.onNext(bookResponse.meta)
+                self?.isEnd = bookResponse.meta.isEnd
+                print(bookResponse.meta, self?.endScroll)
+                if !bookResponse.meta.isEnd && self?.endScroll == true {
+                    do {
+                        guard let currentBooks = try self?.searchedBookSubject.value() else { return }
+                        self?.searchedBookSubject.onNext(currentBooks + bookResponse.documents)
+                    } catch {
+                        print("현재 searchedBookSubject 가져오기 실패: \(error)")
+                        self?.searchedBookSubject.onNext(bookResponse.documents)
+                    }
+                } else {
+                    self?.searchedBookSubject.onNext(bookResponse.documents)
+                }
+                self?.isLoading = false
             }, onFailure: { [weak self] error in
                 self?.searchedBookSubject.onError(error)
             }).disposed(by: disposeBag)
