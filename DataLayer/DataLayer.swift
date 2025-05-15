c480aaa1a0c1de7a638a39c97621b894890d9d4b
EllGHT <rmsgh1188@gmail.com>
2025-05-15 17:39:47 +0900

    chore: Layer별 Framework 생성

---
+++ DataLayer/DataLayer.swift
@@ -0,0 +1,9 @@
+//
+//  DataLayer.swift
+//  DataLayer
+//
+//  Created by 정근호 on 5/15/25.
+//
+
+import Foundation
+
---
+++ DomainLayer/DomainLayer.swift
@@ -0,0 +1,9 @@
+//
+//  DomainLayer.swift
+//  DomainLayer
+//
+//  Created by 정근호 on 5/15/25.
+//
+
+import Foundation
+
