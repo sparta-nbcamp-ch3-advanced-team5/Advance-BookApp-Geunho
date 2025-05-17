c92db91ed47734f1039a9c9f90af0ac8aa326801
EllGHT <rmsgh1188@gmail.com>
2025-05-17 17:43:42 +0900

    feat: DIContainer 생성

---
+++ BookApp/Application/DIContainer.swift
@@ -0,0 +1,54 @@
+//
+//  DIContainer.swift
+//  BookApp
+//
+//  Created by 정근호 on 5/17/25.
+//
+
+import Foundation
+import DataLayer
+import DomainLayer
+import PresentationLayer
+import CoreData
+import UIKit
+
+struct DIContainer {
+    
+    // API
+    let bookResponseRepository: BookResponseRepositoryProtocol
+    // CoreData
+    let cartCoreDataRepository: CartCoreDataRepositoryProtocol
+    let recentBookCoreDataRepository: RecentBookCoreDataRepositoryProtocol
+
+    init(context: NSManagedObjectContext) {
+        self.bookResponseRepository = BookResponseRepository()
+        self.cartCoreDataRepository = CartCoreDataRepository(context: context)
+        self.recentBookCoreDataRepository = RecentBookCoreDataRepository(context: context)
+    }
+
+    func makeSearchViewController() -> SearchViewController {
+    
+        let viewModel = SearchViewModel(
+            bookReponseRepository: bookResponseRepository,
+            recentBookCoreDataRepository: recentBookCoreDataRepository
+        )
+        return SearchViewController(viewModel: viewModel)
+    }
+    
+    func makecartViewController() -> CartViewController {
+        
+        let viewModel = CartViewModel(
+            cartCoreDataRepository: cartCoreDataRepository
+        )
+        
+        return CartViewController(viewModel: viewModel)
+    }
+    
+    func makeInfoViewController(book: Book) -> InfoViewController {
+        let viewModel = InfoViewModel(
+            book: book,
+            cartCoreDataRepository: cartCoreDataRepository,
+            recentBookCoreDataRepository: recentBookCoreDataRepository
+        )
+    }
+}
